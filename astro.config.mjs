73c3eb6549fe983097ad7070f8c1d6e89bdf7a01
Anderson Productions <thibautandphotos@outlook.com>
2025-05-29 19:08:23 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import github from '@astrojs/github';
 
 export default defineConfig({
   site: 'https://andersonproductions.github.io/YOUR_REPO_NAME',
-  base: '/YOUR_REPO_NAME/', // ← use '/' if deploying to username.github.io
+  base: '/',
   adapter: github(),
   integrations: [tailwind()],
 });
