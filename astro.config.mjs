baea106b2372e20349200340a340d509de849dc8
Anderson Productions <thibautandphotos@outlook.com>
2025-05-29 19:14:25 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import github from '@astrojs/github';
 
 export default defineConfig({
-  site: 'https://andersonproductions.github.io/YOUR_REPO_NAME',
+  site: 'https://andersonproductions.github.io',
   base: '/',
   adapter: github(),
   integrations: [tailwind()],
