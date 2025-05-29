a664f8f299374af1b68208fbabf49fe6cc6f16bb
Anderson Productions <thibautandphotos@outlook.com>
2025-05-29 18:57:15 +0000

    Update astro.configv2.mjs

--- astro.configv2.mjs
+++ astro.configv2.mjs
@@ -1,1 +1,10 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import github from '@astrojs/github';
 
+export default defineConfig({
+  site: 'https://andersonproductions.github.io/YOUR_REPO_NAME',
+  base: '/YOUR_REPO_NAME/', // ← use '/' if deploying to username.github.io
+  adapter: github(),
+  integrations: [tailwind()],
+});
