d7d71723b768f66b94b95e544f4f643164c92f83
Killian O'Gorman <83577130+killianogorman@users.noreply.github.com>
2025-06-06 15:22:45 +0100

    Start with svelte project

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
 	import '../app.css';
-	import {gsap,SplitText} from 'gsap/all';
+	
 	let { children } = $props();
-	onMount(()=>{
-	gsap.registerPlugin(SplitText)
-	})
 </script>
 
 {@render children()}
